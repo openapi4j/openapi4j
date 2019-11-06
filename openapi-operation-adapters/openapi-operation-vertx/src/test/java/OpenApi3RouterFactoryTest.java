77982ed0c77801409dd241ebde277c631c50a1df
Rémy NOËL <llfbandit@gmail.com>
2019-11-06 18:29:58 +0100

    feat : add secure handlers tests

--- openapi-operation-adapters/openapi-operation-vertx/src/test/java/OpenApi3RouterFactoryTest.java
+++ openapi-operation-adapters/openapi-operation-vertx/src/test/java/OpenApi3RouterFactoryTest.java
@@ -18,6 +18,7 @@ import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 @RunWith(VertxUnitRunner.class)
 public class OpenApi3RouterFactoryTest extends VertxTestBase {
@@ -28,13 +29,18 @@ public class OpenApi3RouterFactoryTest extends VertxTestBase {
     URL specPath = OpenApi3RouterFactoryTest.class.getResource("/api.yaml");
 
     Async async = context.async();
-    OpenApi3RouterFactory.create(vertx, specPath,
-      result -> {
+    OpenApi3RouterFactory.create(vertx, specPath).setHandler(result -> {
         context.assertTrue(result.succeeded());
         context.assertNotNull(result.result());
         async.complete();
       });
     async.await();
+  }
+
+  @Test(expected = ResolutionException.class)
+  public void mountWrongOperationId(TestContext context) throws ResolutionException {
+    loadSpec(context, "/api.yaml");
+    routerFactory.addOperationHandler("wrong_op", null, RoutingContext::next);
   }
 
   @Test
@@ -53,7 +59,7 @@ public class OpenApi3RouterFactoryTest extends VertxTestBase {
 
   @Test
   public void mountSimpleHandlerTest(TestContext context) throws Exception {
-    loadDefaultSpec(context);
+    loadSpec(context, "/api.yaml");
 
     routerFactory.addOperationHandler("simple", rc -> {
       RequestParameters rqParameters = rc.get("rqParameters");
@@ -73,7 +79,7 @@ public class OpenApi3RouterFactoryTest extends VertxTestBase {
 
   @Test
   public void mountRegexHandlerTest(TestContext context) throws Exception {
-    loadDefaultSpec(context);
+    loadSpec(context, "/api.yaml");
 
     routerFactory.addOperationHandler("regex", rc -> {
       RequestParameters rqParameters = rc.get("rqParameters");
@@ -96,7 +102,7 @@ public class OpenApi3RouterFactoryTest extends VertxTestBase {
 
   @Test
   public void mountLabelMatrixHandlerTest(TestContext context) throws Exception {
-    loadDefaultSpec(context);
+    loadSpec(context, "/api.yaml");
 
     ObjectNode labelObject = JsonNodeFactory.instance.objectNode();
     labelObject.put("name", "Alex");
@@ -129,8 +135,110 @@ public class OpenApi3RouterFactoryTest extends VertxTestBase {
     testRequest(context, HttpMethod.GET, "/.role,123,name,Alex/;matrix=3,4,5/foo/", 200, "OK");
   }
 
-  private void loadDefaultSpec(TestContext context) {
-    URL specPath = OpenApi3RouterFactoryTest.class.getResource("/api.yaml");
+  @Test(expected = ResolutionException.class)
+  public void missingSecuredHandlerTest(TestContext context) throws ResolutionException {
+    loadSpec(context, "/apiSecured.yaml");
+
+    routerFactory.addOperationHandler("secured", rc -> rc
+      .response()
+      .setStatusCode(200)
+      .setStatusMessage("OK")
+      .end());
+
+    routerFactory.getRouter();
+  }
+
+  @Test
+  public void failedSecuredHandlerTest(TestContext context) throws ResolutionException {
+    loadSpec(context, "/apiSecured.yaml");
+
+    routerFactory.addOperationHandler("secured", rc -> rc
+      .response()
+      .setStatusCode(200)
+      .setStatusMessage("OK")
+      .end());
+
+    // Fake failed validation
+    routerFactory.addSecurityHandler("adminAuth", rc -> rc.fail(401));
+
+    Router router = routerFactory.getRouter();
+
+    startServer(context, vertx, router);
+    testRequest(context, HttpMethod.GET, "/secured", 401, "Unauthorized");
+  }
+
+  @Test
+  public void successfulSecuredHandlerTest(TestContext context) throws ResolutionException {
+    loadSpec(context, "/apiSecured.yaml");
+
+    routerFactory.addOperationHandler("secured", rc -> rc
+      .response()
+      .setStatusCode(200)
+      .setStatusMessage("OK")
+      .end());
+
+    // Fake validation
+    routerFactory.addSecurityHandler("adminAuth", RoutingContext::next);
+
+    Router router = routerFactory.getRouter();
+
+    startServer(context, vertx, router);
+    testRequest(context, HttpMethod.GET, "/secured", 200, "OK");
+  }
+
+  @Test(expected = ResolutionException.class)
+  public void missingOAuthSecuredHandlerTest(TestContext context) throws ResolutionException {
+    loadSpec(context, "/apiOAuthSecured.yaml");
+
+    routerFactory.addOperationHandler("secured", rc -> rc
+      .response()
+      .setStatusCode(200)
+      .setStatusMessage("OK")
+      .end());
+
+    routerFactory.getRouter();
+  }
+
+  @Test
+  public void failedOAuthSecuredHandlerTest(TestContext context) throws ResolutionException {
+    loadSpec(context, "/apiOAuthSecured.yaml");
+
+    routerFactory.addOperationHandler("secured", rc -> rc
+      .response()
+      .setStatusCode(200)
+      .setStatusMessage("OK")
+      .end());
+
+    // Fake failed validation
+    routerFactory.addSecurityScopedHandler("OAuth", "read:pets", rc -> rc.fail(401));
+
+    Router router = routerFactory.getRouter();
+
+    startServer(context, vertx, router);
+    testRequest(context, HttpMethod.GET, "/secured", 401, "Unauthorized");
+  }
+
+  @Test
+  public void successfulOAuthSecuredHandlerTest(TestContext context) throws ResolutionException {
+    loadSpec(context, "/apiOAuthSecured.yaml");
+
+    routerFactory.addOperationHandler("secured", rc -> rc
+      .response()
+      .setStatusCode(200)
+      .setStatusMessage("OK")
+      .end());
+
+    // Fake validation
+    routerFactory.addSecurityScopedHandler("OAuth", "read:pets", RoutingContext::next);
+
+    Router router = routerFactory.getRouter();
+
+    startServer(context, vertx, router);
+    testRequest(context, HttpMethod.GET, "/secured", 200, "OK");
+  }
+
+  private void loadSpec(TestContext context, String path) {
+    URL specPath = OpenApi3RouterFactoryTest.class.getResource(path);
 
     Async async = context.async();
     OpenApi3RouterFactory.create(vertx, specPath, result -> {
