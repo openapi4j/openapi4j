769bfe136d3680eaa8c35377e198ac28cc677af9
Rémy NOËL <llfbandit@gmail.com>
2019-11-06 17:47:52 +0100

    feat : improve tests

--- openapi-operation-adapters/openapi-operation-vertx/src/test/java/OpenApi3RouterFactoryTest.java
+++ openapi-operation-adapters/openapi-operation-vertx/src/test/java/OpenApi3RouterFactoryTest.java
@@ -1,10 +1,15 @@
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import org.json.JSONException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openapi4j.core.exception.ResolutionException;
 import org.openapi4j.operation.validator.adapters.server.vertx.v3.OpenApi3RouterFactory;
 import org.openapi4j.operation.validator.adapters.server.vertx.v3.impl.RequestParameters;
+import org.skyscreamer.jsonassert.JSONCompare;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 
 import java.net.URL;
 
@@ -47,31 +52,10 @@ public class OpenApi3RouterFactoryTest extends VertxTestBase {
   }
 
   @Test
-  public void mountHandlerTest(TestContext context) throws Exception {
-    URL specPath = OpenApi3RouterFactoryTest.class.getResource("/api.yaml");
+  public void mountSimpleHandlerTest(TestContext context) throws Exception {
+    loadSpec(context);
 
-    Async async = context.async();
-    OpenApi3RouterFactory.create(vertx, specPath, result -> {
-      routerFactory = result.result();
-      async.complete();
-    });
-
-    async.await();
-
-    routerFactory.addOperationHandler("list-searchable-fields", rc -> {
-      RequestParameters rqParameters = rc.get("rqParameters");
-      context.assertNotNull(rqParameters);
-      context.assertEquals(JsonNodeFactory.instance.textNode("foo"), rqParameters.getPathParameter("dataset"));
-      context.assertEquals(JsonNodeFactory.instance.textNode("bar"), rqParameters.getPathParameter("version"));
-
-      rc
-        .response()
-        .setStatusCode(200)
-        .setStatusMessage("OK")
-        .end();
-    });
-
-    routerFactory.addOperationHandler("records", rc -> {
+    routerFactory.addOperationHandler("simple", rc -> {
       RequestParameters rqParameters = rc.get("rqParameters");
       context.assertNotNull(rqParameters);
       rc
@@ -84,7 +68,76 @@ public class OpenApi3RouterFactoryTest extends VertxTestBase {
     Router router = routerFactory.getRouter();
 
     startServer(context, vertx, router);
+    testRequest(context, HttpMethod.GET, "/simple", 200, "OK");
+  }
+
+  @Test
+  public void mountRegexHandlerTest(TestContext context) throws Exception {
+    loadSpec(context);
+
+    routerFactory.addOperationHandler("regex", rc -> {
+      RequestParameters rqParameters = rc.get("rqParameters");
+      context.assertNotNull(rqParameters);
+      context.assertEquals(JsonNodeFactory.instance.textNode("foo"), rqParameters.getPathParameter("dataset"));
+      context.assertEquals(JsonNodeFactory.instance.textNode("bar"), rqParameters.getPathParameter("version"));
+
+      rc
+        .response()
+        .setStatusCode(200)
+        .setStatusMessage("OK")
+        .end();
+    });
+
+    Router router = routerFactory.getRouter();
+
+    startServer(context, vertx, router);
     testRequest(context, HttpMethod.GET, "/fixed/foo/fixed/bar/fields/", 200, "OK");
-    testRequest(context, HttpMethod.GET, "/records", 200, "OK");
+  }
+
+  @Test
+  public void mountLabelMatrixHandlerTest(TestContext context) throws Exception {
+    loadSpec(context);
+
+    ObjectNode labelObject = JsonNodeFactory.instance.objectNode();
+    labelObject.put("name", "Alex");
+    labelObject.put("role", 123);
+
+    ArrayNode matrixArray = JsonNodeFactory.instance.arrayNode();
+    matrixArray.add("4").add("5").add("3");
+
+    routerFactory.addOperationHandler("label-matrix", rc -> {
+      RequestParameters rqParameters = rc.get("rqParameters");
+      context.assertNotNull(rqParameters);
+
+      try {
+        JSONCompare.compareJSON(labelObject.toString(), rqParameters.getPathParameter("label").toString(), JSONCompareMode.STRICT);
+        JSONCompare.compareJSON(matrixArray.toString(), rqParameters.getPathParameter("matrix").toString(), JSONCompareMode.STRICT);
+      } catch (JSONException e) {
+        context.fail(e);
+      }
+
+      rc
+        .response()
+        .setStatusCode(200)
+        .setStatusMessage("OK")
+        .end();
+    });
+
+    Router router = routerFactory.getRouter();
+
+    startServer(context, vertx, router);
+    testRequest(context, HttpMethod.GET, "/.role,123,name,Alex/;matrix=3,4,5/foo/", 200, "OK");
+  }
+
+  private void loadSpec(TestContext context) {
+    URL specPath = OpenApi3RouterFactoryTest.class.getResource("/api.yaml");
+
+    Async async = context.async();
+    OpenApi3RouterFactory.create(vertx, specPath, result -> {
+      routerFactory = result.result();
+      async.complete();
+    });
+
+    async.await();
   }
 }
