cfc18ea73a1524fc91c9f3e1809711e2747dbf81
Rémy NOËL <llfbandit@gmail.com>
2020-03-03 20:24:45 +0100

    fix: take less memory to track visited elements

--- openapi-parser/src/main/java/org/openapi4j/parser/validation/ValidationContext.java
+++ openapi-parser/src/main/java/org/openapi4j/parser/validation/ValidationContext.java
@@ -3,26 +3,17 @@ package org.openapi4j.parser.validation;
 import org.openapi4j.core.model.OAI;
 import org.openapi4j.core.validation.ValidationResults;
 
-import java.util.IdentityHashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ValidationContext<O extends OAI> {
-  private final Map<Object, Object> visitedObjects = new IdentityHashMap<>();
+  private final Set<Integer> visitedElements = new HashSet<>();
 
   public <V> void validate(O api, V value, final Validator<O, V> validator, ValidationResults results) {
-    if (isVisited(value)) {
+    if (!visitedElements.add(value.hashCode())) {
       return;
     }
 
     validator.validate(this, api, value, results);
-  }
-
-  private boolean isVisited(Object o) {
-    if (visitedObjects.containsKey(o)) {
-      return true;
-    } else {
-      visitedObjects.put(o, o);
-      return false;
-    }
   }
 }
