8651d352ab06b5e3ee3ddf12dbef5ef96f620762
Rémy NOËL <llfbandit@gmail.com>
2020-03-27 15:34:24 +0100

    feat: allOf now returns an error if there's no schema in the schema collection.

--- openapi-schema-validator/src/main/java/org/openapi4j/schema/validator/v3/AllOfValidator.java
+++ openapi-schema-validator/src/main/java/org/openapi4j/schema/validator/v3/AllOfValidator.java
@@ -2,10 +2,12 @@ package org.openapi4j.schema.validator.v3;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import org.openapi4j.core.model.v3.OAI3;
+import org.openapi4j.core.validation.ValidationResult;
 import org.openapi4j.core.validation.ValidationResults;
 import org.openapi4j.schema.validator.ValidationContext;
 
 import static org.openapi4j.core.model.v3.OAI3SchemaKeywords.ALLOF;
+import static org.openapi4j.core.validation.ValidationSeverity.ERROR;
 
 /**
  * allOf keyword validator.
@@ -15,6 +17,9 @@ import static org.openapi4j.core.model.v3.OAI3SchemaKeywords.ALLOF;
  * <a href="https://tools.ietf.org/html/draft-wright-json-schema-validation-00#page-11" />
  */
 class AllOfValidator extends DiscriminatorValidator {
+  private static final ValidationResult ERR = new ValidationResult(ERROR, 1001, "Schema description is erroneous. allOf should have at least 1 element.");
+  private static final ValidationResults.CrumbInfo CRUMB_INFO = new ValidationResults.CrumbInfo(ALLOF, true);
+
   static AllOfValidator create(ValidationContext<OAI3> context, JsonNode schemaNode, JsonNode schemaParentNode, SchemaValidator parentSchema) {
     return new AllOfValidator(context, schemaNode, schemaParentNode, parentSchema);
   }
@@ -25,6 +30,11 @@ class AllOfValidator extends DiscriminatorValidator {
 
   @Override
   public void validateWithoutDiscriminator(final JsonNode valueNode, final ValidationResults results) {
+    if (schemas.isEmpty()) {
+      results.add(CRUMB_INFO, ERR);
+      return;
+    }
+
     validate(() -> {
       for (SchemaValidator schema : schemas) {
         schema.validateWithContext(valueNode, results);
