3730f45f5c77924bcdffca0cedb90ad47f034e92
Rémy NOËL <llfbandit@gmail.com>
2020-03-29 01:22:06 +0100

    core: remove unused imports

--- openapi-operation-validator/src/main/java/org/openapi4j/operation/validator/util/convert/style/StyleConverter.java
+++ openapi-operation-validator/src/main/java/org/openapi4j/operation/validator/util/convert/style/StyleConverter.java
@@ -2,8 +2,6 @@ package org.openapi4j.operation.validator.util.convert.style;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-
-import org.openapi4j.core.exception.ResolutionException;
 import org.openapi4j.operation.validator.util.convert.TypeConverter;
 import org.openapi4j.parser.model.v3.AbsParameter;
 import org.openapi4j.parser.model.v3.Schema;
--- openapi-schema-validator/src/main/java/org/openapi4j/schema/validator/v3/NullableValidator.java
+++ openapi-schema-validator/src/main/java/org/openapi4j/schema/validator/v3/NullableValidator.java
@@ -2,7 +2,6 @@ package org.openapi4j.schema.validator.v3;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import org.openapi4j.core.model.v3.OAI3;
-import org.openapi4j.core.validation.ValidationException;
 import org.openapi4j.core.validation.ValidationResult;
 import org.openapi4j.core.validation.ValidationResults;
 import org.openapi4j.schema.validator.BaseJsonValidator;
