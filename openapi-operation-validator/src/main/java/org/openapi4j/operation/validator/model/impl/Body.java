45128fc509de8e72fdde8851bacdc04401c4ae64
smhc <shanehird@gmail.com>
2020-03-30 20:42:19 +1000

    Use Body.from(Object) instead of Body.from(Map<String,Object>) (#87)

--- openapi-operation-validator/src/main/java/org/openapi4j/operation/validator/model/impl/Body.java
+++ openapi-operation-validator/src/main/java/org/openapi4j/operation/validator/model/impl/Body.java
@@ -8,41 +8,40 @@ import org.openapi4j.parser.model.v3.MediaType;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Map;
 
 import static java.util.Objects.requireNonNull;
 
 public class Body {
   private static final String BODY_REQUIRED_ERR_MSG = "Body content is required.";
 
-  private final Map<String, Object> bodyMap;
+  private final Object bodyObject;
   private final JsonNode bodyNode;
   private final String bodyStr;
   private final InputStream bodyIs;
 
-  private Body(Map<String, Object> body) {
-    this.bodyMap = body;
+  private Body(Object body) {
+    this.bodyObject = body;
     this.bodyNode = null;
     this.bodyStr = null;
     this.bodyIs = null;
   }
 
   private Body(JsonNode bodyNode) {
-    this.bodyMap = null;
+    this.bodyObject = null;
     this.bodyNode = bodyNode;
     this.bodyStr = null;
     this.bodyIs = null;
   }
 
   private Body(String body) {
-    this.bodyMap = null;
+    this.bodyObject = null;
     this.bodyNode = null;
     this.bodyStr = body;
     this.bodyIs = null;
   }
 
   private Body(InputStream bodyIs) {
-    this.bodyMap = null;
+    this.bodyObject = null;
     this.bodyNode = null;
     this.bodyStr = null;
     this.bodyIs = bodyIs;
@@ -61,13 +60,13 @@ public class Body {
   }
 
   /**
-   * Constructs a body from the given map tree model.
+   * Constructs a body from the given Java Object.
    * This is the preferred way to build a body wrapper.
    *
-   * @param body the given map tree model (JSON, XML, ...)
+   * @param body the given model as a Java Object (JSON, XML, ...)
    * @return The constructed body.
    */
-  public static Body from(Map<String, Object> body) {
+  public static Body from(Object body) {
     requireNonNull(body, BODY_REQUIRED_ERR_MSG);
     return new Body(body);
   }
@@ -76,7 +75,7 @@ public class Body {
    * Constructs a body from the given string.
    * This is a convenient method to build a body wrapper.
    * Also, you should definitely at look at {@link Body#from(JsonNode)} or
-   * {@link Body#from(Map)} for performance.
+   * {@link Body#from(Object)} for performance.
    *
    * @param body The given body string.
    * @return The constructed body.
@@ -90,7 +89,7 @@ public class Body {
    * Constructs a body from the given stream.
    * This is a convenient method to build a body wrapper.
    * Also, you should definitely at look at {@link Body#from(JsonNode)} or
-   * {@link Body#from(Map)} for performance.
+   * {@link Body#from(Object)} for performance.
    *
    * @param body The given body stream
    * @return The constructed body.
@@ -104,8 +103,8 @@ public class Body {
                                    final String rawContentType) throws IOException {
     if (bodyNode != null) {
       return bodyNode;
-    } else if (bodyMap != null) {
-      return TreeUtil.json.convertValue(bodyMap, JsonNode.class);
+    } else if (bodyObject != null) {
+      return TreeUtil.json.convertValue(bodyObject, JsonNode.class);
     } else {
       return ContentConverter.convert(mediaType, rawContentType, bodyIs, bodyStr);
     }
